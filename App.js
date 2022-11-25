35dc1aec34bf4dc38ac11f7df907917139892de2
chadhaaa <chadha.hadji@gmail.com>
2022-11-25 19:15:15 +0100

    ChadhaHajji_Crud_Statistics

--- App.js
+++ App.js
@@ -1,14 +1,9 @@
-// import AllCompetences from "./screens/crudCompetence/AllCompetences";
-// import AddCompetences from "./screens/crudCompetence/AddCompetence";
-// import UpdateCompetences from "./screens/crudCompetence/UpdateCompetence";
-// import InvitePlayer from "./screens/invitePlayer/InvitePlayer";
 import Home from "./screens/Home/Home";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CrudStatistics from "./screens/crudStatistics/CrudStatistics";
 
 const Stack = createNativeStackNavigator();
-// const tab = createNativeStackNavigator();
 
 const App = () => {
   return (
@@ -19,15 +14,6 @@ const App = () => {
           component={Home}
           options={{ title: "Welcome To Dashboard !" }}
         />
-        {/* <Stack.Screen
-          name="Competences"
-          component={AllCompetences}
-          options={{ title: "Welcome To Competences !" }}
-        />
-        <Stack.Screen name="AddCompetences" component={AddCompetences} />
-        <Stack.Screen name="UpdateCompetences" component={UpdateCompetences} />
-
-        <Stack.Screen name="InvitePlayer" component={InvitePlayer} /> */}
         <Stack.Screen name="Statistics" component={CrudStatistics} />
       </Stack.Navigator>
     </NavigationContainer>
