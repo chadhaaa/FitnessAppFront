b8b86486319349faff6541a5ec405914790bbfd0
chadhaaa <chadha.hadji@gmail.com>
2022-12-12 12:33:43 +0100

    Still trying to filter

--- screens/SessionDetails/GetAllSessions.js
+++ screens/SessionDetails/GetAllSessions.js
@@ -7,6 +7,7 @@ import { useIsFocused } from "@react-navigation/native";
 import CalendarPicker from "react-native-calendar-picker";
 import moment from "moment";
 import Calendar from "react-native-calendar-range-picker";
+import DatePicker, { getFormatedDate } from "react-native-modern-datepicker";
 
 const GetSeance = ({ navigation }) => {
   const [sessions, setSession] = useState([]);
@@ -37,6 +38,7 @@ const GetSeance = ({ navigation }) => {
             onDateChange={onDateNowChange}
             // allowRangeSelection={true}
           />
+          <DatePicker mode="datepicker" onDateChange={onDateNowChange} />
           <Button title="Filter" onPress={onDateNowChange} />
         </View>
         {/* <View>
@@ -49,7 +51,7 @@ const GetSeance = ({ navigation }) => {
               <Seance
                 id={session._id}
                 key={session._id}
-                day={moment(session.day).format("DD MM YYYY").toString()}
+                day={moment(session.day).format("YYYY/MM/DD").toString()}
                 idPlace={session.idPlace}
                 cancellation={session.cancellation}
                 reason={session.reason}
